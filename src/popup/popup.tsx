20c68ca4f235178941d1bf506bb29df18f18200b
kshashwat007 <kshashwat007@gmail.com>
2023-10-01 13:28:05 +0530

    update popup.tsx

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -3,10 +3,16 @@ import ReactDom from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import './popup.css'
 
-const test = <img src='icon.png' />
+const App: React.FC<{}> = () => {
+  return (
+    <div>
+      <img src='icon.png'/>
+    </div>
+  )
+}
 
 
 const container = document.createElement('div')
 document.body.appendChild(container)
 const root = createRoot(container)
-root.render(test)
+root.render(<App />)
